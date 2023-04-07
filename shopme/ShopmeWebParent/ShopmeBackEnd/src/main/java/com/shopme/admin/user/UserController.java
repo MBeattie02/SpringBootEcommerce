d67bcc0430ddbf4baab48d11f676ca5a21a11848
MBeattie02 <91721899+MBeattie02@users.noreply.github.com>
2023-04-07 14:44:25 +0100

    New user function

--- shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -2,10 +2,13 @@ package com.shopme.admin.user;
 
 import java.util.List;
 
+import com.shopme.common.entity.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.common.entity.User;
 
@@ -21,5 +24,28 @@ public class UserController {
         model.addAttribute("listUsers", listUsers);
 
         return "users";
+    }
+
+    @GetMapping("/users/new")
+    public String newUser(Model model) {
+        List<Role> listRoles = service.listRoles();
+
+        User user = new User();
+        user.setEnabled(true);
+
+        model.addAttribute("user", user);
+        model.addAttribute("listRoles", listRoles);
+
+        return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes redirectAttributes) {
+        System.out.println(user);
+        service.save(user);
+
+        redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+
+        return "redirect:/users";
     }
 }
