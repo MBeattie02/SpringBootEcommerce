81e4d306b877de5febd3773196d7559efa1569c1
MBeattie02 <91721899+MBeattie02@users.noreply.github.com>
2023-04-09 10:02:09 +0100

    Update user function

--- shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRestController.java
+++ shopme/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
         private UserService service;
 
         @PostMapping("/users/check_email")
-        public String checkDuplicateEmail(@Param("email") String email) {
-            return service.isEmailUnique(email) ? "OK" : "Duplicated";
+        public String checkDuplicateEmail(@Param("id") Integer id, @Param("email") String email) {
+            return service.isEmailUnique(id, email) ? "OK" : "Duplicated";
         }
 }
